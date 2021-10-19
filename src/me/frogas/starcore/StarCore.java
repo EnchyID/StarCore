9fbb780c62ac665b3bb14fe6729bc4936717943a
FrogasQ <91828204+FrogasQ@users.noreply.github.com>
2021-10-19 20:07:19 +0800

    Update StarCore.java

--- src/me/frogas/starcore/StarCore.java
+++ src/me/frogas/starcore/StarCore.java
@@ -3,6 +3,7 @@ package me.frogas.starcore;
 import cn.nukkit.Player;
 import cn.nukkit.plugin.PluginBase;
 import cn.nukkit.event.Listener;
+import cn.nukkit.item.Item;
 import cn.nukkit.event.player.PlayerInteractEvent;
 import cn.nukkit.event.player.PlayerJoinEvent;
 
@@ -11,5 +12,21 @@ public class StarCore extends PluginBase implements Listener {
     public void onEnable(){
         this.getServer().getPluginManager().registerEvents(this, this);
         this.getServer().getLogger().info("Enable by @Frogas");
+    }
+
+    public void onJoin(PlayerJoinEvent event){
+        final Player player = event.getPlayer();
+        inventory = player.getInventory();
+        inventory.clearAll();
+        inventory.setItem(0, Item.get(264, 0, 1).setCustomName("MY ITEM"));
+    }
+
+    public void onQuit(PlayerQuitEvent event){
+        final Player player = event.getPlayer();
+        itemHand = player.getInventory().getItemInHand();
+        if(itemHand.getCustomName() == "MY ITEM"){
+            player.sendMessage("You've clicked MY ITEM!");
+            return true;
+        }
     }
 }
